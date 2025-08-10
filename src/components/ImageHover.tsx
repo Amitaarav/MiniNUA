56300624e6359e23a867f429d1703e0a730feb50
Amit Kumar <aaravamit555@gmail.com>
2025-08-10 08:03:20 +0530

    enhanced UI

---
+++ src/components/ImageHover.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const ImageHover = ({
+  src,
+  hoverSrc,
+  alt,
+}: {
+  src: string
+  hoverSrc: string
+  alt: string
+}) => {
+  const [isHovered, setIsHovered] = useState(false)
+  let hoverTimeout: any
+  const handleMouseEnter = () => {
+    hoverTimeout = setTimeout(() => setIsHovered(true), 1000) // 1 second delay
+  }
+
+  const handleMouseLeave = () => {
+    clearTimeout(hoverTimeout)
+    setIsHovered(false)
+  }
+
+  return (
+    <div
+      className='relative h-52'
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img
+        src={src}
+        alt={alt}
+        sizes='80vw'
+        className={`object-contain transition-opacity duration-500 ${
+          isHovered ? 'opacity-0' : 'opacity-100'
+        }`}
+      />
+      <img
+        src={hoverSrc}
+        alt={alt}
+        sizes='80vw'
+        className={`absolute inset-0 object-contain transition-opacity duration-500 ${
+          isHovered ? 'opacity-100' : 'opacity-0'
+        }`}
+      />
+    </div>
+  )
+}
+
+export default ImageHover
